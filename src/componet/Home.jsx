b2eee6fdaf655f18fb8880270667f2fab07b5eba
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-11-16 07:12:45 -0700

    mar

--- src/componet/Home.jsx
+++ src/componet/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='max-w-full mx-auto grid md:grid-cols-2 gap-12 px-20 justify-center items-center'>
 
       <div className='md:w-[40rem]'>
-        <h1 className='md:font-bold text-xl text-start mt-20 font-medium pb-6'>The decentralized power of the blockchain to combat and eradicate conterfeit product.</h1>
+        <h1 className='md:font-bold text-xl text-start mt-20 font-medium mb-4'>The decentralized power of the blockchain to combat and eradicate conterfeit product.</h1>
         <p className='md:pt-4 text-start text-md'>The rise in sale of counterfeit product in Africa has become an overwhelming problem. 
         In Nigeria alone, NAFDAC in 2002 found that nearly 41% of pharmaceuticals were counterfeit, 
         and 70% were unregistered. Though there have been efforts to combat the presence of counterfeit drugs,
