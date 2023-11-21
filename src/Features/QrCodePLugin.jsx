276c514f56a299c0fb310b88bf51d395911ec264
ceasar28 <emmanuelugochukwuekete@gmail.com>
2023-11-21 18:34:54 +0100

    scanner

--- src/Features/QrCodePLugin.jsx
+++ src/Features/QrCodePLugin.jsx
@@ -1,58 +1,54 @@
 import { Html5QrcodeScanner } from "html5-qrcode";
-import React from "react";
+import { useEffect } from "react";
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
-class Html5QrcodePlugin extends React.Component {
-  render() {
-    return <div id={qrcodeRegionId} />;
+// Creates the configuration object for Html5QrcodeScanner.
+const createConfig = (props) => {
+  let config = {};
+  if (props.fps) {
+    config.fps = props.fps;
   }
-
-  componentWillUnmount() {
-    // TODO(mebjas): See if there is a better way to handle
-    //  promise in `componentWillUnmount`.
-    this.html5QrcodeScanner.clear().catch((error) => {
-      console.error("Failed to clear html5QrcodeScanner. ", error);
-    });
+  if (props.qrbox) {
+    config.qrbox = props.qrbox;
   }
+  if (props.aspectRatio) {
+    config.aspectRatio = props.aspectRatio;
+  }
+  if (props.disableFlip !== undefined) {
+    config.disableFlip = props.disableFlip;
+  }
+  return config;
+};
 
-  componentDidMount() {
-    // Creates the configuration object for Html5QrcodeScanner.
-    function createConfig(props) {
-      let config = {};
-      if (props.fps) {
-        config.fps = props.fps;
-      }
-      if (props.qrbox) {
-        config.qrbox = props.qrbox;
-      }
-      if (props.aspectRatio) {
-        config.aspectRatio = props.aspectRatio;
-      }
-      if (props.disableFlip !== undefined) {
-        config.disableFlip = props.disableFlip;
-      }
-      return config;
-    }
-
-    let config = createConfig(this.props);
-    let verbose = this.props.verbose === true;
-
+const Html5QrcodePlugin = (props) => {
+  useEffect(() => {
+    // when component mounts
+    const config = createConfig(props);
+    const verbose = props.verbose === true;
     // Suceess callback is required.
-    if (!this.props.qrCodeSuccessCallback) {
+    if (!props.qrCodeSuccessCallback) {
       throw "qrCodeSuccessCallback is required callback.";
     }
-
-    this.html5QrcodeScanner = new Html5QrcodeScanner(
+    const html5QrcodeScanner = new Html5QrcodeScanner(
       qrcodeRegionId,
       config,
       verbose
     );
-    this.html5QrcodeScanner.render(
-      this.props.qrCodeSuccessCallback,
-      this.props.qrCodeErrorCallback
+    html5QrcodeScanner.render(
+      props.qrCodeSuccessCallback,
+      props.qrCodeErrorCallback
     );
-  }
-}
+
+    // cleanup function when component will unmount
+    return () => {
+      html5QrcodeScanner.clear().catch((error) => {
+        console.error("Failed to clear html5QrcodeScanner. ", error);
+      });
+    };
+  }, []);
+
+  return <div id={qrcodeRegionId} />;
+};
 
 export default Html5QrcodePlugin;
